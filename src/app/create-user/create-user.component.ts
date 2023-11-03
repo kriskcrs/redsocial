bc7cc51647b90a85ceafa204354b915f1eb28b2f
Pablin117 <tgelma117@gmail.com>
2023-11-03 17:57:55 -0600

    componente de crear usuario

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -12,6 +12,12 @@ import {MatDatepickerModule} from '@angular/material/datepicker'
 import {NgIf} from '@angular/common';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {catchError} from "rxjs/operators";
+import {throwError} from "rxjs";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {AppComponent} from "../app.component";
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -25,6 +31,10 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./create-user.component.css'],
 })
 export class CreateUserComponent {
+  constructor(private _snackBar: MatSnackBar, private url: AppComponent, private router: Router, private http: HttpClient) { }
+
+  path = this.url.url
+  dataCreate: any={}
   hide = true;
   file: any
   imageSrc: string | ArrayBuffer | null = null;
@@ -34,6 +44,9 @@ export class CreateUserComponent {
 
   matcher = new MyErrorStateMatcher();
 
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action);
+  }
   onFileSelected(event: any) {
     const selectedFile = event.target.files[0];
     if (selectedFile) {
@@ -50,4 +63,43 @@ export class CreateUserComponent {
     // Hacer clic en el input de tipo archivo para abrir el cuadro de diálogo de selección de archivo
     this.fileInput.nativeElement.click();
   }
+
+  userCreation() {
+    let formularioValido: any = document.getElementById("add");
+    if (formularioValido.reportValidity()) {
+      this.userCreationRequest().subscribe(
+        (response: any) => this.userCreationResponse(response)
+      )
+
+    }  }
+  userCreationRequest() {
+    return this.http.post<any>(this.path + "/createUser", this.dataCreate, { observe: 'response' }).pipe(
+      catchError((error: any) => {
+        if (error.status === 400) {
+          // error para parámetros inválidos
+          this.openSnackBar(error.error.message, "Aceptar");
+        } else {
+          // error de conexión o un 500
+          this.openSnackBar("No existe conexión con el servidor", "Aceptar");
+        }
+        return throwError(error);
+      })
+    )
+  }
+
+  userCreationResponse(response: any) {
+if (response.status === 200) {
+  this.openSnackBar("Usuario creado correctamente", "Aceptar");
+    }
+    else {
+      console.log(`Recibí un código de estado inesperado: ${response.status}`);
+    }
+
+  }
+
+  login(){
+    this.router.navigateByUrl("/")
+  }
+
+
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -140,7 +140,7 @@ export class LoginComponent {
 
   //para registrar al usuario
   registryService() {
-
+    this.router.navigateByUrl("/createUser")
   }
 
   //metodo diego
