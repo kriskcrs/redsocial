f0221ec6e152cbcf4310170f48a9b1534ec70a66
Pablin117 <tgelma117@gmail.com>
2023-11-07 14:04:36 -0600

    mensaje y retorno de crear publicacion

--- src/app/create-publication/create-publication.component.ts
+++ src/app/create-publication/create-publication.component.ts
@@ -16,7 +16,7 @@ export class CreatePublicationComponent {
 
   ngOnInit() {
     this.validateSession();
-  
+
   }
 
   //vars
@@ -36,10 +36,11 @@ export class CreatePublicationComponent {
   file: any
   imageSrc: string | ArrayBuffer | null = null;
   @ViewChild('fileInput') fileInput: any;
-  urlImages: any = "/Users/cristiancaceres/WebstormProjects/redsocial/src/assets"
+  urlImages: any = "C:\\Users\\josue\\WebstormProjects\\redsocial\\src\\assets"
   serve: any = 10.10
   hide = true;
   dataCreate: any = {}
+  idPhot:any=""
 
 
    formData = new FormData();
@@ -127,8 +128,8 @@ export class CreatePublicationComponent {
      }
    }
    publicationsRequest() {
-    console.log(this.dataCreate);
-    
+    this.dataCreate.userIdUser=this.dataUser.idUser
+     this.dataCreate.photoIdPhoto=this.idPhot
      return this.http.post<any>(this.path + "/createPublication",this.dataCreate, { observe: 'response' }).pipe(
        catchError((error: any) => {
            if (error.status === 400) {
@@ -144,9 +145,11 @@ export class CreatePublicationComponent {
    }
    publicationsResponse(response: any) {
     console.log(response);
-    
+     this.openSnackBar("Publicacion creada", "Aceptar");
+     this.router.navigateByUrl("/home")
+
    }
- 
+
 
 
   //imagenes
@@ -178,16 +181,6 @@ export class CreatePublicationComponent {
    formData.append('server', server)
    formData.append('path', path)
    formData.append('user', user)
-   
-   console.log(formData)
-   console.log(path);
-   console.log(user);
-   console.log(server);
-   
-   
-   
-
-console.log(this.path);
 
     return this.http.post<any>(this.path + "/fileUp", formData, { observe: 'response' }).pipe(
       catchError((error: any) => {
@@ -203,12 +196,15 @@ console.log(this.path);
       ))
   }
   imagenResponse(response: any) {
-  
-   console.log("respondio el servicio de imagen ");
-   console.log(response);
-   
+    const rutaOriginal = response.body.ruta_original;
+    const partes = rutaOriginal.split('original\\');
+    this.idPhot = partes[1];
+    this.publicationsService()
+
   }
 
-
+home(){
+  this.router.navigateByUrl("/home")
+}
 
 }
