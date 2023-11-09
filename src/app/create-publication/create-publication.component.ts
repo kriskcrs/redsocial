932e3bc402cd779a91977aea6fdde0f30f9cae85
ricardochavz <ricardo.taracena64@gmail.com>
2023-11-08 21:07:00 -0600

    cambios de funcionalidades y visuales

--- src/app/create-publication/create-publication.component.ts
+++ src/app/create-publication/create-publication.component.ts
@@ -35,9 +35,9 @@ export class CreatePublicationComponent {
   file: any
   imageSrc: string | ArrayBuffer | null = null;
   @ViewChild('fileInput') fileInput: any;
-  urlImages: any = "C:\\Users\\josue\\WebstormProjects\\redsocial\\src\\assets"
+  //urlImages: any = "C:\\Users\\josue\\WebstormProjects\\redsocial\\src\\assets"
   //urlImages: any = "/Users/cristiancaceres/WebstormProjects/redsocial/src/assets"
-  //urlImages: any = "C:\\Users\\ricar\\OneDrive\\Escritorio\\TAREAS\\Desarrollo\\redsocial\\src\\assets"
+  urlImages: any = "C:\\Users\\ricar\\OneDrive\\Escritorio\\TAREAS\\Desarrollo\\redsocial\\src\\assets"
   serve: any = 10.10
   hide = true;
   dataCreate: any = {}
--- src/app/publication/publication.component.ts
+++ src/app/publication/publication.component.ts
@@ -45,7 +45,7 @@ export class PublicationComponent {
   file: any
   imageSrc: string | ArrayBuffer | null = null;
   @ViewChild('fileInput') fileInput: any;
-  urlImages: any = "C:\\Users\\josue\\WebstormProjects\\redsocial\\src\\assets"
+  urlImages: any = "C:\\Users\\ricar\\OneDrive\\Escritorio\\TAREAS\\Desarrollo\\redsocial\\src\\assets"
   serve: any = 10.10
   hide = true;
   dataCreate: any = {}
