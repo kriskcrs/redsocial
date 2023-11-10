f04c49afeb9b13ffff1003f7b1f92f3d82f5282c
ccaceres <ccaceres@bytesw.com>
2023-11-10 14:46:57 -0600

    cambios visuales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'redsocial';
-  // url = "http://localhost:4042/v1"
-  url = "http://3.84.225.189:4042/v1"
+   url = "http://localhost:4042/v1"
+  //url = "http://3.84.225.189:4042/v1"
   //urlImages: any = "C:\\Users\\josue\\WebstormProjects\\redsocial\\src\\assets"
-  // urlImages: any = "/Users/cristiancaceres/WebstormProjects/redsocial/src/assets"
+   urlImages: any = "/Users/cristiancaceres/WebstormProjects/redsocial/src/assets"
   // urlImages: any = "/Users/diboy/Documents/UMG/8vo Semestre/WD/redsocial/src/assets"
   // urlImages: any = "C:\\Users\\ricar\\OneDrive\\Escritorio\\TAREAS\\Desarrollo\\redsocial\\src\\assets"
 
-  urlImages: any = "/home/ubuntu/redsocial/src/assets"
+  //urlImages: any = "/home/ubuntu/redsocial/src/assets"
 }
