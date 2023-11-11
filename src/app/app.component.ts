ffe200a8a9a4e40836b256ab572d45819ea90948
ccaceres <ccaceres@bytesw.com>
2023-11-11 10:46:10 -0600

    cambio de ip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'redsocial';
   // url = "http://localhost:4042/v1"
-  //url = "http://3.84.225.189:4042/v1"
-  url = "http://72.44.44.121:4042/v1"
+    url = "http://100.26.143.249:4042/v1"
+  //url = "http://72.44.44.121:4042/v1"
   //urlImages: any = "C:\\Users\\josue\\WebstormProjects\\redsocial\\src\\assets"
   // urlImages: any = "/Users/cristiancaceres/WebstormProjects/redsocial/src/assets"
   // urlImages: any = "/Users/diboy/Documents/UMG/8vo Semestre/WD/redsocial/src/assets"
   // urlImages: any = "C:\\Users\\ricar\\OneDrive\\Escritorio\\TAREAS\\Desarrollo\\redsocial\\src\\assets"
 
-  //urlImages: any = "/home/ubuntu/redsocial/src/assets"
-  urlImages: any = "/home/ec2-user"
+  urlImages: any = "/home/ubuntu/redsocial/src/assets"
+  //urlImages: any = "/home/ec2-user"
 
 }
